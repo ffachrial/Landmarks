21fa853da2ba676200ef1ff5016ba534fb8f5ade
Firman Fachrial <firman.fachrial@gmail.com>
2022-07-25 21:45:38 +0700

    Section 1 Display a User Profile

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Firman Fachrial on 25/07/22.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotification = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "ffachrial")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String {rawValue}
+    }
+}
