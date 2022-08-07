c6d8b475e18878e3b31cf4e4dc2c22d136fdb342
Firman Fachrial <firman.fachrial@gmail.com>
2022-08-07 07:46:29 +0700

    Section 1 Add a WatchOS Target

---
+++ WatchLandmarks WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Firman Fachrial on 07/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
